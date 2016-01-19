6cd47b0ed60f0fd54c2731feda654463b88b0d43
djachenko <i.s.djachenko@gmail.com>
2016-01-19 14:42:16 +0600

    Heroku needs it

--- list/urls.py
+++ list/urls.py
@@ -7,12 +7,13 @@ from booklist import settings
 __author__ = 'justin'
 
 urlpatterns = [
-    url(r'^$', views.booklist, name='booklist'),
+  url(r'^$', views.booklist, name='booklist'),
 
-    url(r'^book/', include('list.bookurls')),
-    url(r'^storage/', include('list.storageurls')),
-    url(r'^author/', include('list.authorurls')),
-    url(r'^publisher/', include('list.publisherurls')),
-    url(r'^import/*$', views.import_data, name="import"),
-    url(r'^check$', views.check_import_state, name="check"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  url(r'^book/', include('list.bookurls')),
+  url(r'^storage/', include('list.storageurls')),
+  url(r'^author/', include('list.authorurls')),
+  url(r'^publisher/', include('list.publisherurls')),
+  url(r'^import/*$', views.import_data, name="import"),
+  url(r'^check$', views.check_import_state, name="check"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
