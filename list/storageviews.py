2a8b9d2ab9cf61ec89e02d2f98b598f2fb8bb320
djachenko <i.s.djachenko@gmail.com>
2016-01-22 17:15:20 +0600

    DRY implemented

--- list/storageviews.py
+++ list/storageviews.py
@@ -1,63 +1,27 @@
-from django.core.urlresolvers import reverse_lazy
-from django.http import Http404
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
-
+from list.entityviews import EntityDetail, EntityNew, EntityEdit, EntityDelete
 from list.models import Storage, Book
-from list.views import BaseContextMixin
 
-FORM_TITLE = "storage"
-
-
-def add_common_context(context):
-    context.update({
-        "form_title": FORM_TITLE
-    })
-
-    return context
+STORAGE_FORM_TITLE = "storage"
 
 
-class StorageDetail(DetailView, BaseContextMixin):
+class StorageDetail(EntityDetail):
     model = Storage
-    template_name = "list/named_detail.html"
 
-    def get_object(self, queryset=None):
-        try:
-            return super().get_object(queryset)
-        except Http404:
-            return None
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        requested_storage = self.object
-
-        if requested_storage:
-            books_in_storage = Book.objects.filter(storage=requested_storage)
-
-            context["required"] = books_in_storage.count() > 0
-            context["books"] = books_in_storage
-
-        return context
+    def related_books(self):
+        return Book.objects.filter(storage=self.object)
 
 
-class StorageNew(CreateView):
+class StorageNew(EntityNew):
     model = Storage
     fields = ("name",)
-    template_name = "list/named_edit.html"
-
-    def get_context_data(self, **kwargs):
-        return add_common_context(super().get_context_data(**kwargs))
+    form_title = STORAGE_FORM_TITLE
 
 
-class StorageEdit(UpdateView):
+class StorageEdit(EntityEdit):
     model = Storage
     fields = ("name",)
-    template_name = "list/named_edit.html"
-
-    def get_context_data(self, **kwargs):
-        return add_common_context(super().get_context_data(**kwargs))
+    form_title = STORAGE_FORM_TITLE
 
 
-class StorageDelete(DeleteView):
+class StorageDelete(EntityDelete):
     model = Storage
-    success_url = reverse_lazy("booklist")
